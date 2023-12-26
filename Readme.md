906c156ffaf9a9a7de9ef35c00dd605c00b02f28
Akash Kumar <Aakashkkr04@gmail.com>
2023-12-26 12:41:02 +0530

    first commit

---
+++ Readme.md
@@ -0,0 +1,48 @@
+# Weather Web App
+
+This Weather Web App is a simple and user-friendly application developed using HTML, CSS, and JavaScript. It allows users to retrieve real-time weather information by entering the name of a city. The app provides details such as temperature, humidity, and wind speed.
+
+## Features
+
+- **City-Based Weather Information:** Users can enter the name of a city to get up-to-date weather details.
+
+- **Temperature Display:** The application shows the current temperature of the specified city.
+
+- **Humidity Information:** Users can view the humidity level in the selected city.
+
+- **Wind Speed:** The app provides information on the current wind speed in the specified location.
+
+## Live Preview
+
+You can try the Weather Web App [here](#). Follow the link to access the live version of the application.
+
+## Technologies Used
+
+- **HTML:** Markup language for structuring the web page.
+
+- **CSS:** Stylesheet language for designing a visually appealing and responsive interface.
+
+- **JavaScript:** Programming language for fetching weather data and updating the user interface dynamically.
+
+## How to Use
+
+1. Open the Weather Web App in a web browser.
+
+2. Enter the name of the city for which you want to check the weather.
+
+3. Click the "search" button.
+
+4. The app will display the current temperature, humidity, and wind speed for the specified city.
+
+## Dependencies
+
+No external libraries or frameworks are required for this project.
+
+## Support and Contribution
+
+If you encounter any issues or have suggestions for improvement, feel free to open an issue or submit a pull request.
+
+## License
+
+This Weather Web App is released under the [MIT License](LICENSE). You are free to use, modify, and distribute the code as per the terms of the license.
+
---
+++ script/app.js
@@ -0,0 +1,82 @@
+let key = "8ff0464ec000d26745455d55a167d9b3";
+let baseUrl = "https://api.openweathermap.org/data/2.5/weather?q="
+let input = document.querySelector(".search input");
+let searchbtn = document.querySelector(".search button");
+let tempText = document.querySelector(".temp");
+let weather = document.querySelector(".weather-text");
+let weatherImg = document.querySelector(".weather");
+let locationT = document.querySelector(".location-name");
+let app = document.querySelector(".app");
+
+
+const tempUpdate = async() =>{
+  let cityName = input.value;
+
+  let url = `${baseUrl}${cityName}&appid=${key}`;
+  let code = await fetch(url);
+  let json = await code.json();
+
+  //error in input city
+  console.log(code)
+  if(code.status === 404 || cityName === ""){
+    document.querySelector(".error").style.display = "flex";
+    document.querySelector(".weatherDisplay").style.display = "none";
+  }else{
+    document.querySelector(".weatherDisplay").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  }
+
+  let temp = (json.main.temp) - 273.15; //converting the api temprature in celcius because it give in kelvin
+  console.log(url);
+  //weather temprature change
+  tempText.innerText = `${temp.toFixed(1)}°C`;
+
+  // weather text 
+  let weatherT = json.weather[0].main;
+  weather.innerText = `${weatherT}`
+
+  //weather image changing condition
+  if(weatherT === "Clear"){
+    weatherImg.src = `resources/clear.png`;
+  }else if(weatherT === "Fog"){
+    weatherImg.src = `resources/mist.png`;
+  }else if(weatherT === "Clouds"){
+    weatherImg.src = `resources/clouds.png`;
+  }else if(weatherT === "Rain"){
+    weatherImg.src = `resources/rain.png`;
+    app.setAttribute('style', 'background-color: #000000d3');
+  }else if(weatherT === "Drizzle"){
+    weatherImg.src = `resources/drizzle.png`
+  }else if(weatherT === "Snow"){
+    weatherImg.src = `resources/snow.png`
+    app.setAttribute('style', 'background-color: #113f59')
+  }
+
+  // writing the location name on screen
+  locationT.innerText = `${json.name}`
+  //humidity and wind speed 
+
+  let humidity = document.querySelector("#humidity span");
+  humidity.innerText = `${json.main.humidity}%`
+
+  let windSpeedT = document.querySelector("#wind span");
+  let windSpeed = json.wind.speed*3.6;
+  windSpeedT.innerText = `${windSpeed.toFixed(2)} km/h`
+}
+
+
+
+searchbtn.addEventListener("click", async()=>{
+  tempUpdate();
+})
+
+addEventListener("keypress", (evt)=>{
+  if(evt.key === "Enter"){
+    evt.preventDefault();
+    searchbtn.click();
+  }
+});
+
+window.addEventListener("load", ()=>{
+  tempUpdate();
+})
